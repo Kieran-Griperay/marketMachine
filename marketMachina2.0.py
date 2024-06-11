eaa89e0158537ca0838095f7d1ce7454638da9f8
Kieran-Griperay <124720154+Kieran-Griperay@users.noreply.github.com>
2024-06-10 21:08:51 -0400

    Update marketMachina2.0.py

--- marketMachina2.0.py
+++ marketMachina2.0.py
@@ -1,24 +1,35 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from sklearn.model_selection import train_test_split
-import yfinance as yf
-import pandas as pd
-import numpy as np
-import talib
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import LSTM, Dense, Dropout
-from keras.callbacks import EarlyStopping
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import joblib
 import tensorflow as tf
 from tensorflow.keras import backend as K
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+import joblib
 import os
+import numpy as np
+import pandas as pd
+import talib
+import yfinance as yf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from functools import lru_cache
 import datetime
 import concurrent.futures
-import Stock_Filtration as sf
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+# Ensure TensorFlow is using the GPU
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+# Set memory growth to avoid using all GPU memory at once
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
 
 # Constants
 N_STEPS = 50
@@ -27,6 +38,7 @@ FUTURE_STEP = [1, 2, 3, 4, 5]
 models = {}
 scalers = {}
 tf.get_logger().setLevel('ERROR')
+
 # Determine the current week number
 now = datetime.datetime.now()
 todays_date = datetime.date.today() 
@@ -36,11 +48,8 @@ month_number = todays_date.month
 
 # Load data
 filtered_symbols_df = pd.read_csv('Stock Filtration/Backtesting/Results/Best_Stocks_Today.csv')
-print(filtered_symbols_df)
 tickers = filtered_symbols_df['Ticker'].tolist()
-workers = len(tickers) 
-print(f"Workers: {workers}")
-
+workers = len(tickers)
 
 dfs = {}
 for t in tickers:
@@ -72,29 +81,17 @@ def get_current_price(ticker):
     else:
         return None
 
-
-
 @lru_cache(maxsize=128)
 def get_yesterday_price(tick):
     ticker = yf.Ticker(tick)
     historical_data = ticker.history(period='2d')
     return historical_data['Close'].iloc[-2]
 
-# Function to calculate technical indicators
 def add_technical_indicators(df):
-    """
-    Calculate and append technical indicators to the DataFrame.
-    Parameters:
-    df (DataFrame): The input DataFrame containing stock prices.
-    Returns:
-    DataFrame: The DataFrame with added technical indicators.
-    """
-    df['Average_Price'] = (df['Open'] + df['High'] + df['Low'] + df['Close']) / 4
     df['RSI'] = talib.RSI(df['Close'], timeperiod=28)
     df['ADX'] = talib.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
-    df['EMA_50'] = talib.EMA(df['Close'], timeperiod=50)*1.5 # weight
-    df['EMA_90'] = talib.EMA(df['Close'], timeperiod = 90) * 1.5
-    df['EMA_200'] = talib.EMA(df['Close'], timeperiod = 200)*1.5
+    df['EMA_50'] = talib.EMA(df['Close'], timeperiod=50)
+    df['EMA_90'] = talib.EMA(df['Close'], timeperiod = 90) 
     df['MACD'], df['MACD_signal'], df['MACD_hist'] = talib.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
     df['BB_upper'], df['BB_middle'], df['BB_lower'] = talib.BBANDS(df['Close'], timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
     df['BB_middle'] = df['BB_middle'] * 1.75
@@ -105,17 +102,7 @@ def add_technical_indicators(df):
     df.dropna(inplace=True)
     return df
 
-
 def create_dataset(X, Y, n_steps):
-    """
-    Creates dataset for LSTM model training.
-    Parameters:
-    X (array): Input features.
-    Y (array): Target output.
-    n_steps (int): Number of steps/lookback period.
-    Returns:
-    tuple: Tuple containing feature and target datasets.
-    """
     Xs, Ys = [], []
     for i in range(len(X) - n_steps):
         Xs.append(X[i:(i + n_steps)])
@@ -123,14 +110,10 @@ def create_dataset(X, Y, n_steps):
     return np.array(Xs), np.array(Ys)
 
 def showNewModelPredictions(dfs, ticker, model, scalers):
-    # Base directory to save plots
     base_directory = "plots/"
-
     df = dfs[ticker]
     scaler_feature, scaler_target = scalers
     fig_name = f"{year_number}_{ticker}_"
-
-    # Ensure scalers are fit even if just loading models
     columns_to_scale = df.drop(['Close', 'Predicted_Close'], axis=1, errors='ignore')
     features = scaler_feature.transform(columns_to_scale)
     target = scaler_target.transform(df[['Close']])
@@ -138,7 +121,6 @@ def showNewModelPredictions(dfs, ticker, model, scalers):
         pathy = str(base_directory) + str(fig_name) + "predictions.png"
         img = mpimg.imread(pathy)
     except (IOError, FileNotFoundError):
-        # Predicting past using features
         predicted_close_prices = []
         for i in range(len(df) - N_STEPS):
             if len(df) < N_STEPS:
@@ -149,12 +131,10 @@ def showNewModelPredictions(dfs, ticker, model, scalers):
             pred_price = scaler_target.inverse_transform(pred_scaled)[0][0]
             predicted_close_prices.append(pred_price)
 
-            # Append predictions to DataFrame
-            prediction_series = pd.Series(data=np.nan, index=df.index)
-            prediction_series[-len(predicted_close_prices):] = predicted_close_prices
-            df['Predicted_Close'] = prediction_series
+        prediction_series = pd.Series(data=np.nan, index=df.index)
+        prediction_series[-len(predicted_close_prices):] = predicted_close_prices
+        df['Predicted_Close'] = prediction_series
 
-            # Plotting
         plt.figure(figsize=(14, 7))
         plt.plot(df['Close'], label='Actual Closing Prices')
         plt.plot(df.index[-len(predicted_close_prices):], predicted_close_prices, label='Predicted Closing Prices', linestyle='--')
@@ -162,38 +142,18 @@ def showNewModelPredictions(dfs, ticker, model, scalers):
         plt.xlabel('Date')
         plt.ylabel('Close Price')
         plt.legend()
-        # Save the figure
         plt.savefig(os.path.join(base_directory, f"{fig_name}predictions.png"))
         plt.close()
 
 def calculate_percentage_increase(ticker, prix, predicted_closing):
-    """
-    Calculates the predicted percentage increase for the chosen stocks, return the best ones.
-    Parameters: 
-    ticker (str): used for the accessing of dfs dataframe.
-    predicted closing price (ndarray): will be used for the predicted percentage increase
-    Returns: 
-    float: representing the increase or decrease of the stock
-    """
-    if not predicted_closing.size:  # Check if predicted_price is empty
+    if not predicted_closing.size:
         raise ValueError("Predicted price is empty")
     predicted_value = predicted_closing[0, 0]
     predicted_percent = (predicted_value - prix) / prix
     return predicted_percent * 100
 
 def check_data_consistency(data_frames):
-    """
-    Checks the consistency of feature and row counts across multiple data frames.
-    Parameters:
-    data_frames (dict): A dictionary with tickers as keys and pandas DataFrame as values.
-    Returns:
-    dict: A dictionary reporting the number of features and rows for each ticker and any inconsistencies.
-    """
     info_dict = {}
-    standard_features = None
-    standard_rows = None
-    inconsistencies = []
-
     for ticker, df in data_frames.items():
         features = df.shape[1]
         rows = df.shape[0]
@@ -201,15 +161,6 @@ def check_data_consistency(data_frames):
     return info_dict
 
 def build_and_train_model(ticker, data, n_steps, test_size=0.1, random_state=42):
-    """
-    Builds and trains LSTM model.
-    Parameters:
-    ticker (str): The ticker symbol.
-    data (DataFrame): The DataFrame containing training data.
-    n_steps (int): Number of timesteps to look back.
-    Returns:
-    tuple: Model and scaler objects, validation loss.
-    """
     K.clear_session()
     scaler_feature = MinMaxScaler()
     scaler_target = MinMaxScaler()
@@ -221,7 +172,7 @@ def build_and_train_model(ticker, data, n_steps, test_size=0.1, random_state=42)
         raise ValueError("Not enough data for training and validation.")
 
     X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=test_size, random_state=random_state)
-    batch_size = min(8, len(X_train))  # Ensure batch size does not exceed the number of training samples
+    batch_size = min(8, len(X_train))
     model = Sequential([
         LSTM(60, return_sequences=True, input_shape=(n_steps, X_train.shape[2])),
         Dropout(0.3),
@@ -236,28 +187,19 @@ def build_and_train_model(ticker, data, n_steps, test_size=0.1, random_state=42)
                         validation_data=(X_val, Y_val), callbacks=[early_stopping])
 
     val_loss = history.history.get('val_loss', [None])[-1]
-    # Calculate evaluation metrics
     val_predictions = model.predict(X_val)
     mae = mean_absolute_error(Y_val, val_predictions)
     mse = mean_squared_error(Y_val, val_predictions)
     rmse = np.sqrt(mse)
     
-    # Save metrics to a text file
     with open('model_metrics.txt', 'a') as f:
         f.write(f"Ticker: {ticker} | Month: {month_number} | Val Loss: {val_loss} | MAE: {mae} | MSE: {mse} | RMSE: {rmse}\n")
 
-
     return model, scaler_feature, scaler_target, val_loss, mae, mse, rmse
 
-
-# Prepare data with technical indicators
 for ticker, df in dfs.items():
     dfs[ticker] = add_technical_indicators(df)
 
-# Add fundamental indicators and recommendations to each DataFsrame
-# for ticker, df in dfs.items():
-#     dfs[ticker] = add_fundamental_indicators(df, ticker)
-# Model training and prediction
 get_action = lambda predicted_price, current_price: "BUY" if predicted_price > current_price + current_price * .005 else "HOLD"
 
 def process_ticker(ticker, predicted_price):
@@ -297,10 +239,9 @@ def train_model_for_ticker(ticker, df):
 
 percentage_increase = {}
 if __name__ == '__main__':
-
     with ThreadPoolExecutor(max_workers=workers) as executor:
         futures = {executor.submit(train_model_for_ticker, ticker, df): ticker for ticker, df in dfs.items()}
-        for future in concurrent.futures.as_completed(futures):  # Timeout after 600 seconds
+        for future in concurrent.futures.as_completed(futures):
             ticker = futures[future]
             try:
                 result = future.result()
